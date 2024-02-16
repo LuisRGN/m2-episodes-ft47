bbd849f83d10c1c59b9b1dc3a38b75dd8b34dd9d
LuisRGN <luisrafael1598@gmail.com>
2024-02-15 19:56:31 -0500

    se avanza hasta la parte 3

---
+++ src/utils/dataBase.js
@@ -0,0 +1,17 @@
+const dataBase = {
+    allUsers: [
+        {
+            name: "Rick",
+            email: "rick@gmail.com",
+            password: "1234",
+        }
+    ],
+    allEpisodes: [
+        {
+            id: 2,
+            name: "Nombre del Episodio",
+            episode: "Season 01 - Episode 02",
+            completed: false || true,
+        }
+    ]
+}
